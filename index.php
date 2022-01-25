44ee230eedc84306ee0f9650631488bd69a6d359
Illensky <alexis.laroche.02240@gmail.com>
2022-01-25 15:27:40 +0100

    Exercice done

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  * 2. Affichez la valeur de ce cookie avec un simple echo.
  */
 // TODO Votre code ici.
-
-
+/* setcookie("username","Alexis", time() +3600);
+/* echo $_COOKIE['username'];
 
 /**
  * 3. Commentez le code du point 1 et 2
@@ -14,3 +14,7 @@
  * 5. Créez une page bio.php et tentez d'afficher le cookie username sur cette page.
  */
 // TODO Votre code ici.
+
+setcookie("username","Laroche", time() +3600 *50, '/', 'localhost');
+?>
+<a href="bio.php">bio</a>
